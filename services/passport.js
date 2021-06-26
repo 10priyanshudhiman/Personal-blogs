22b7d7660fb0377380ef9fb08b3fdb413ab38033
Priyanshu Dhiman <10priyanshudhiman@gmail.com>
2021-06-26 06:38:03 +0530

    t

--- services/passport.js
+++ services/passport.js
@@ -22,7 +22,8 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientId,
   clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback'
+  callbackURL: '/auth/google/callback',
+  proxy: true
 
 }, (accessToken, refreshToken, profile, done) => {
 
